abaac2fe94b6fb82cd48f7fe01ff553839c41285
unknown <mujaheedsami13@gmail.com>
2022-12-14 11:41:31 +0530

    server

---
+++ readme.js
@@ -0,0 +1,17 @@
+const baseUrl = "http://localhost:3000/";
+const productsData = "http://localhost:3000/api/products";
+const usersData = "http://localhost:3000/api/users";
+
+// steps to use server :-
+// => install node modules in backend folder [npm i];
+// => run "node server.js" command in the terminal;
+
+
+// -------------------------------------------------
+
+// To add the data use => POST method
+
+// To edit or update data 
+// use => PATCH method
+
+// To delete use => DELETE 
